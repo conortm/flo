60b2636b1ffef701aaac7e9f81450261d3e19bf3
Elijah Lynn <elijah@elijahlynn.net>
2015-11-05 08:33:12 -0500

    Use static instead of self.

--- src/flo/Command/FeaturesOverriddenChecker.php
+++ src/flo/Command/FeaturesOverriddenChecker.php
@@ -26,9 +26,9 @@ class FeaturesOverriddenChecker extends Command {
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
     $gh_status_post = FALSE;
-    $targetRef = getenv(self::GITHUB_PULL_REQUEST_COMMIT);
-    $targetURL = getenv(self::JENKINS_BUILD_URL);
-    $pullRequest = getenv(self::GITHUB_PULL_REQUEST_ID);
+    $targetRef = getenv(static::GITHUB_PULL_REQUEST_COMMIT);
+    $targetURL = getenv(static::JENKINS_BUILD_URL);
+    $pullRequest = getenv(static::GITHUB_PULL_REQUEST_ID);
     $github = $this->getGithub();
 
     // Check if we're going to post to GH or not.
